05d7d17ab651c9f7224296292e7c557da95ad429
ty201457 <ty201457@gmail.com>
2025-04-03 08:44:05 +0800

    fix: bind to 0.0.0.0 for Render

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import csv
 import io
 import threading
 import webbrowser
+import os
 
 app = Flask(__name__)
 
@@ -105,5 +106,6 @@ def open_browser():
     webbrowser.open('http://127.0.0.1:5000')
 
 if __name__ == '__main__':
-    threading.Timer(1.0, open_browser).start()
-    app.run()
+    port = int(os.environ.get("PORT", 5000))  # Render 會提供 PORT 環境變數
+    app.run(host='0.0.0.0', port=port)
+
